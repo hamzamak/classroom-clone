53db41ef6dace8cf2af0ef68a6e6b9aee4bb53ca
hamzamak <makhkhas.hamza@gmail.com>
2023-03-30 14:14:58 +0000

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducers } from './reducers';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducers, compose(applyMiddleware(thunk) ));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
