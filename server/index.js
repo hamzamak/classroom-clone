fa99c68f9f5ce87e4b7bac3ae42f859041327643
hamzamak <yahyahamza50@gmail.com>
2023-05-21 21:08:15 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ import chapitresRoutes from './routes/chapitres.js'
 import dotenv from "dotenv";
 //__dirname and __filename aren’t shared either, but import.meta.url is dans ES MODULE
 import * as url from 'url';
-//const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
@@ -19,7 +18,7 @@ dotenv.config();
 app.use('/uploads/themes' , express.static(__dirname +'/uploads/themes'));
 app.use(bodyParser.json({limit : "30mb" ,extended : true}))
 app.use(bodyParser.urlencoded({limit : "30mb" ,extended : true}))
-//assurer que cette appel vient first avant les routes 
+
 app.use(cors())
 app.use(morgan('tiny'))
 //*************************************** */ 
@@ -34,8 +33,10 @@ const MONGOOSE_URL = process.env.MONGOOSE_URL
 
 const PORT = process.env.PORT || 5000 ;
 
-
+mongoose.set("strictQuery", false);
 mongoose.connect(MONGOOSE_URL , {useNewUrlParser : true , useUnifiedTopology : true})
 .then(()=> app.listen(PORT, ()=> console.log(`Server Running from port ${PORT}`)))
 .catch((err)=> console.log(err.message) )
 
+
+// unitil here we make sure that we 're not getting any message warning in console 
